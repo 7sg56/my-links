abd31ac857b9753698091a8ce14c030ba273c5b0
Sourish Ghosh <sghosh.ile.7@gmail.com>
2025-08-23 16:55:44 +0530

    next v3 - fixed background

--- my-app/app/page.tsx
+++ my-app/app/page.tsx
@@ -1,18 +1,20 @@
 import React from "react";
-import { Spotlight } from "@/components/ui/spotlight-new";
-import { MinimalCard } from "@/components/ui/minimal-card";
 
 export default function Home() {
   return (
-   <div className="h-screen w-screen flex items-center justify-center bg-black/[0.96] antialiased bg-grid-white/[0.02] relative overflow-hidden">
-      <Spotlight />
-      <div className="p-4 max-w-7xl mx-auto relative z-10 w-full pt-20 md:pt-0">
+   <div className="h-screen w-screen flex items-center justify-center relative overflow-hidden">
+      {/* Stars Background */}
+      <div id="stars"></div>
+      <div id="stars2"></div>
+      <div id="stars3"></div>
+            <div className="p-4 max-w-7xl mx-auto relative z-10 w-full pt-20 md:pt-0">
         <h1 className="text-4xl md:text-7xl font-bold text-center bg-clip-text text-transparent bg-gradient-to-b from-neutral-50 to-neutral-400 bg-opacity-50">
           Sourish Ghosh.
         </h1>
         <p className="mt-4 font-normal text-xl text-base text-neutral-300 max-w-lg text-center mx-auto mb-12">
           Creating the Unimaginable
         </p>
+        
       </div>
     </div>
   );
